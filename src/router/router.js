7919528390cb419a0fa4409df7564f700cf56f83
vincent <likonion@outlook.com>
2017-11-21 17:27:00 +0800

    build update

--- src/router/router.js
+++ src/router/router.js
@@ -4,7 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
+    // mode: 'history',
     // base: '/ulms/office/',
     routes: [
         // 异步加载组件
